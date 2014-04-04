db1cb57bf000067f55c2dee9fe59deb1120b11fa
Wong Cody Johnson <g2areo@cdf.toronto.edu>
2014-04-04 13:57:42 -0400

    q2 works and is fast!

--- q2/q2.py
+++ q2/q2.py
@@ -4,7 +4,7 @@ start_time = time.time()
 
 import sys
 import math
-file = open('test2.dat') #sys.stdin
+file = open('q2_medium.dat') #sys.stdin
 
 
 items = []
@@ -24,28 +24,40 @@ for line in file:
             n = int(line.strip())  
         elif line!='\n':
             lineArray = line.strip().split(' ')
+            #store items as [time@ground, position]
             items.append([int(lineArray[1]) + h, int(lineArray[0])])
 
 # sort elements by arrival to ground time
 items = sorted(items)
 
+# for every element in the list
 for i in range(0, n):
-    time = i[0]
-    position = i[1]
-    if position > time*s:
-        # object is unreachable
+    t = items[i][0] #time
+    p = items[i][1] #position
+    # if object is unreachable, append 0
+    if p > t*s:
         results.append(0)
+    # first item
+    #elif i == 0:
+        #results.append(1)
     else:
-        max = 0;
-        for k in range(n-1,-1):
-            if (k == -1)
-                # there is no compatible items to pick up
-                results.append(1)
-                #distance                 #delta t
-            if (math.fabs(items[k][1] - position) < math.fabs(items[k][0]-time) *s)
+        # find the most recent compatible item
+        for k in range(i-1, -2, -1):
+            # if you've iterated through everything, and nothing is compatible
+            if (k == -1):
+                results.append(1) # append just the one item - 1
+
+            # if compatible interval is found, add the value of it + 1 to results
+                      #delta p                 # delta t      *  s
+            elif math.fabs(items[k][1] - p) <= (math.fabs(items[k][0]-t) * s):
                 results.append(results[k]+1)
+                break;
             
         
 
 
 print(time.time() - start_time, "seconds")
+
+print(results)
+
+print("max: " + str(max(results)))
